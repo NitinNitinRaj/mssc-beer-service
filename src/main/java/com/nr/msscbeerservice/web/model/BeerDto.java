3ae6428046ce71b5573afaa0967480bb254f17fc
bnitinraj <bnitinraj7@gmail.com>
2024-03-11 16:32:50 +0530

    adding wed data model

---
+++ src/main/java/com/nr/msscbeerservice/web/model/BeerDto.java
@@ -0,0 +1,32 @@
+package com.nr.msscbeerservice.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BeerDto {
+    private UUID id;
+    private Integer version;
+
+    private OffsetDateTime createDate;
+
+    private  OffsetDateTime lastModifiedDate;
+
+    private  String beerName;
+
+    private  BeerStyle beerStyle;
+
+    private  Long upc;
+
+    private BigDecimal price;
+    private Integer quantityOnHand;
+}
