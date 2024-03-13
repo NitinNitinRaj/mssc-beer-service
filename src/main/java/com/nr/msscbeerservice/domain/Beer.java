2b6dab3114e30b247bfadc4d21d6b723f16e3c5c
bnitinraj <bnitinraj7@gmail.com>
2024-03-13 10:28:54 +0530

    Adding Beer And beer repository

---
+++ src/main/java/com/nr/msscbeerservice/domain/Beer.java
@@ -0,0 +1,50 @@
+package com.nr.msscbeerservice.domain;
+
+import com.nr.msscbeerservice.web.model.BeerStyle;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Beer {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            type = org.hibernate.id.uuid.UuidGenerator.class
+    )
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    private UUID id;
+
+    @Version
+    private Long version;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createDate;
+
+    @UpdateTimestamp
+    private Timestamp lastModifiedDate;
+    private String beerName;
+    private BeerStyle beerStyle;
+
+    @Column(unique = true)
+    private Long upc;
+    private BigDecimal price;
+    private Integer quantityOnHand;
+}
